67d9bc83a35bf13d4d049adb3511604ba45f88cc
MrrGoodCat <mrrgoodcat@gmail.com>
2016-04-01 19:15:56 +0300

    Controllers folder

---
+++ groupOne/Projects/UniTester/UniTester/Controller/ControllerEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniTester.Controller
+{
+    public class ControllerEventArgs : EventArgs
+    {
+        public ControllerEventArgs(string message)
+        {
+
+        }
+    }
+}
